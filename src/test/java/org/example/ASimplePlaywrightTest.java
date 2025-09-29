2d384af3d5093d0410e68ca94b2d0eafe37c39a0
AkhoMngqibisa <akhomngqibisa@gmail.com>
2025-09-29 19:30:00 +0200

    test - Test Case 1: Check if the website title contains Practice Software Testing

--- src/test/java/org/example/ASimplePlaywrightTest.java
+++ src/test/java/org/example/ASimplePlaywrightTest.java
@@ -1,4 +1,32 @@
 package org.example;
 
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class ASimplePlaywrightTest {
+
+    @Test
+    void shouldShowThePageTitle() {
+        // Create a Playwright environment
+        Playwright playwright = Playwright.create();
+
+        // Create a browser
+        Browser browser = playwright.chromium().launch();
+
+        // Create a page
+        Page page = browser.newPage();
+
+        // Navigate to the page
+        page.navigate("https://practicesoftwaretesting.com");
+
+        // Fetch the page title
+        String pageTitle = page.title();
+
+        // Check what the title contains
+        Assertions.assertTrue(pageTitle.contains("Practice Software Testing"));
+
+    }
 }
