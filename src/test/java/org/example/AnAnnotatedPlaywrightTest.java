50774c3e9f0cb8fb22f3ca4215519a70036e337a
AkhoMngqibisa <akhomngqibisa@gmail.com>
2025-10-01 19:45:00 +0200

    test - Copy previouse test cases to AnAnnotatedPlaywrightTest

--- src/test/java/org/example/AnAnnotatedPlaywrightTest.java
+++ src/test/java/org/example/AnAnnotatedPlaywrightTest.java
@@ -1,4 +1,27 @@
 package org.example;
 
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.junit.UsePlaywright;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+@UsePlaywright
 public class AnAnnotatedPlaywrightTest {
+
+    @Test
+    void shouldShowThePageTitle(Page page) {
+        page.navigate("https://practicesoftwaretesting.com");
+        String pageTitle = page.title();
+        Assertions.assertTrue(pageTitle.contains("Practice Software Testing"));
+    }
+
+    @Test
+    void shouldShowSearchTermsInTheTitle(Page page) {
+        page.navigate("https://practicesoftwaretesting.com");
+        page.locator("[placeholder=Search]").fill("Pliers");
+        page.locator("button:has-text('Search')").click();
+
+        int matchingSearchResults = page.locator(".card").count();
+        Assertions.assertTrue(matchingSearchResults > 0);
+    }
 }
