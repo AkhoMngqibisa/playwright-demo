aff0f037d656e754134f68aea68690f8f3371db4
AkhoMngqibisa <akhomngqibisa@gmail.com>
2025-10-03 20:15:00 +0200

    test - Create a before each method to do set up

--- src/test/java/org/example/PlaywrightLocatorsTest.java
+++ src/test/java/org/example/PlaywrightLocatorsTest.java
@@ -2,6 +2,7 @@ package org.example;
 
 import com.microsoft.playwright.*;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
 import java.util.Arrays;
 
@@ -20,5 +21,11 @@ public class PlaywrightLocatorsTest {
                         .setHeadless(false)
                         .setArgs(Arrays.asList("--no-sandbox","--disable-gpu","--disable-extensions"))
         );
+    }
+
+    @BeforeEach
+    void setup() {
+        browserContext = browser.newContext();
+        page = browserContext.newPage();
     }
 }
