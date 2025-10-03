6f41827ad430b0719f3e6c95e2f4f0fb913f9ae1
AkhoMngqibisa <akhomngqibisa@gmail.com>
2025-10-03 19:45:00 +0200

    test - Declare static variables global

--- src/test/java/org/example/PlaywrightLocatorsTest.java
+++ src/test/java/org/example/PlaywrightLocatorsTest.java
@@ -1,4 +1,14 @@
 package org.example;
 
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
 public class PlaywrightLocatorsTest {
+    protected static Playwright playwright;
+    protected static Browser browser;
+    protected static BrowserContext browserContext;
+
+    Page page;
 }
