b6579e68e9a643d8c779386bac1fcead29b987b7
AkhoMngqibisa <akhomngqibisa@gmail.com>
2025-10-24 20:00:00 +0200

    test - Create a @BeforeAll method that set up a browser

--- src/test/java/org/example/AddingItemsToTheCartTest.java
+++ src/test/java/org/example/AddingItemsToTheCartTest.java
@@ -1,4 +1,23 @@
 package org.example;
 
+import com.microsoft.playwright.*;
+import org.junit.jupiter.api.BeforeAll;
+
+import java.util.Arrays;
+
 public class AddingItemsToTheCartTest {
+    protected static Playwright playwright;
+    protected static Browser browser;
+    protected static BrowserContext browserContext;
+
+    Page page;
+
+    @BeforeAll
+    static void setUpBrowser() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(
+                new BrowserType().LaunchOptions().setHeadless(false)
+                        .setArgs(Arrays.asList("--no-sandbox", "--disable-extensions", "--disable-gpu"))
+        );
+    }
 }
