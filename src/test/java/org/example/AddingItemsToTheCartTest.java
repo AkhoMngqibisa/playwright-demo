48aa5122ad6425bdfc589e65452ff6a7675f5667
AkhoMngqibisa <akhomngqibisa@gmail.com>
2025-10-25 19:30:00 +0200

    refactor - Fix an error on instance of browsertype

--- src/test/java/org/example/AddingItemsToTheCartTest.java
+++ src/test/java/org/example/AddingItemsToTheCartTest.java
@@ -19,7 +19,7 @@ public class AddingItemsToTheCartTest {
     static void setUpBrowser() {
         playwright = Playwright.create();
         browser = playwright.chromium().launch(
-                new BrowserType().LaunchOptions().setHeadless(false)
+                new BrowserType.LaunchOptions().setHeadless(false)
                         .setArgs(Arrays.asList("--no-sandbox", "--disable-extensions", "--disable-gpu"))
         );
     }
