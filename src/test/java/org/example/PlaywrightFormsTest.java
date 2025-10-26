72a53cedea637cfacc164b30ff77c5763c3a830a
AkhoMngqibisa <akhomngqibisa@gmail.com>
2025-10-26 21:30:00 +0200

    test - Configure the test class with required variables

--- src/test/java/org/example/PlaywrightFormsTest.java
+++ src/test/java/org/example/PlaywrightFormsTest.java
@@ -1,4 +1,43 @@
 package org.example;
 
+import com.microsoft.playwright.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.Arrays;
+
 public class PlaywrightFormsTest {
+    protected static Playwright playwright;
+    protected static Browser browser;
+    protected static BrowserContext browserContext;
+    Page page;
+
+    @BeforeAll
+    static void setUpBrowser() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(
+                new BrowserType.LaunchOptions()
+                        .setHeadless(false)
+                        .setArgs(Arrays.asList("--no-sandbox", "--disable-extensions", "--disable-gpu"))
+        );
+    }
+
+    @BeforeEach
+    void setUpBrowserContext() {
+        browserContext = browser.newContext();
+        page = browser.newPage();
+    }
+
+    @AfterEach
+    void tearDownBrowserContext() {
+        browserContext.close();
+    }
+
+    @AfterAll
+    static void tearDownBrowser() {
+        playwright.close();
+        browser.close();
+    }
 }
