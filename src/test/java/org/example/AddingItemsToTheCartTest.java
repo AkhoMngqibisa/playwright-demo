7425b9f7e4428af383fde9554cef41b6d283dc42
AkhoMngqibisa <akhomngqibisa@gmail.com>
2025-10-26 20:30:00 +0200

    test - Centralise the test configuration

--- src/test/java/org/example/AddingItemsToTheCartTest.java
+++ src/test/java/org/example/AddingItemsToTheCartTest.java
@@ -4,51 +4,17 @@ import com.microsoft.playwright.*;
 import com.microsoft.playwright.junit.UsePlaywright;
 import com.microsoft.playwright.options.AriaRole;
 import org.junit.jupiter.api.*;
-import java.util.Arrays;
 import java.util.List;
 import org.assertj.core.api.Assertions;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
-@UsePlaywright
+@UsePlaywright(HeadlessChromeOptions.class)
 public class AddingItemsToTheCartTest {
-    protected static Playwright playwright;
-    protected static Browser browser;
-    protected static BrowserContext browserContext;
-
-    Page page;
-
-    @BeforeAll
-    static void setUpBrowser() {
-        playwright = Playwright.create();
-        browser = playwright.chromium().launch(
-                new BrowserType.LaunchOptions().setHeadless(false)
-                        .setArgs(Arrays.asList("--no-sandbox", "--disable-extensions", "--disable-gpu"))
-        );
-
-        playwright.selectors().setTestIdAttribute("data-test");
-    }
-
-    @BeforeEach
-    void setUp() {
-        browserContext = browser.newContext();
-        page = browser.newPage();
-    }
-
-    @AfterEach
-    void closeContext() {
-        browserContext.close();
-    }
-
-    @AfterAll
-    static void tearDown() {
-        browser.close();
-        playwright.close();
-    }
 
     @DisplayName("Search for pliers")
     @Test
-    void searchForPliers() {
+    void searchForPliers(Page page) {
         page.navigate("https://practicesoftwaretesting.com");
         page.getByPlaceholder("Search").fill("Pliers");
         page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Search")).click();
--- src/test/java/org/example/HeadlessChromeOptions.java
+++ src/test/java/org/example/HeadlessChromeOptions.java
@@ -1,4 +1,18 @@
 package org.example;
 
-public class HeadlessChromeOptions {
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.junit.Options;
+import com.microsoft.playwright.junit.OptionsFactory;
+
+import java.util.Arrays;
+
+public class HeadlessChromeOptions implements OptionsFactory {
+
+    public Options getOptions() {
+        return new Options().setLaunchOptions(
+            new BrowserType.LaunchOptions()
+                    .setHeadless(true)
+                    .setArgs(Arrays.asList("--no-sandbox","--disable-extensions","--disable-gpu"))
+        ).setTestIdAttribute("data-test");
+    }
 }
