cef27271eaffca9fedda12edaf19c0c6b7b8cec4
AkhoMngqibisa <akhomngqibisa@gmail.com>
2025-10-31 20:30:00 +0200

    test - Configure the test class

--- src/test/java/org/example/PlaywrightAssertionsTest.java
+++ src/test/java/org/example/PlaywrightAssertionsTest.java
@@ -1,4 +1,42 @@
 package org.example;
 
+import com.microsoft.playwright.*;
+import org.junit.jupiter.api.*;
+
+import java.util.Arrays;
+
 public class PlaywrightAssertionsTest {
+
+    protected static Playwright playwright;
+    protected static Browser browser;
+    protected static BrowserContext browserContext;
+
+    Page page;
+
+    @BeforeAll
+    static void setupBrowser() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(
+                new BrowserType.LaunchOptions()
+                        .setHeadless(true)
+                        .setArgs(Arrays.asList("--no-sandbox", "--disable-gpu", "--disable-extensions"))
+        );
+    }
+
+    @AfterAll
+    static void teardownBrowser() {
+        browser.close();
+        playwright.close();
+    }
+
+    @BeforeEach
+    void setup() {
+        browserContext = browser.newContext();
+        page = browser.newPage();
+    }
+
+    @AfterEach
+    void tearDown() {
+        browserContext.close();
+    }
 }
