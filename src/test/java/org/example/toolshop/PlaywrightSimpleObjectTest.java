9555789f75af37be16d5f637743a3d26bc8c7eae
AkhoMngqibisa <akhomngqibisa@gmail.com>
2025-11-10 20:00:00 +0200

    test - Setup the test class

--- src/test/java/org/example/toolshop/PlaywrightSimpleObjectTest.java
+++ src/test/java/org/example/toolshop/PlaywrightSimpleObjectTest.java
@@ -1,4 +1,54 @@
 package org.example.toolshop;
 
+import com.microsoft.playwright.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
+
+import java.util.Arrays;
+
+@Execution(ExecutionMode.SAME_THREAD)
 public class PlaywrightSimpleObjectTest {
+
+    protected static Playwright playwright;
+    protected static Browser browser;
+    protected static BrowserContext browserContext;
+
+    Page page;
+
+    @BeforeAll
+    static void setUpBrowser() {
+        playwright = Playwright.create();
+        playwright.selectors().setTestIdAttribute("data-test");
+        browser = playwright.chromium().launch(
+                new BrowserType.LaunchOptions().setHeadless(true)
+                        .setArgs(Arrays.asList("--no-sandbox", "--disable-gpu", "--disable-extensions"))
+
+        );
+    }
+
+    @BeforeEach
+    void setUp() {
+        browserContext = browser.newContext();
+        page = browser.newPage();
+    }
+
+    @AfterEach
+    void closeContext() {
+        browserContext.close();
+    }
+
+    @AfterAll
+    static void tearDownBrowser() {
+        browser.close();
+        playwright.close();
+    }
+
+    @BeforeEach
+    void openHomePage() {
+        page.navigate("https://practicesoftwaretesting.com");
+    }
 }
