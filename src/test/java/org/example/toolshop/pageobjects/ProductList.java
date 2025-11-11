104efaaf30ebbc9fd6e20b61d396e5f78014e089
AkhoMngqibisa <akhomngqibisa@gmail.com>
2025-11-11 20:30:00 +0200

    test - Organise test: ProductList: Add getProductNames

--- src/test/java/org/example/toolshop/pageobjects/ProductList.java
+++ src/test/java/org/example/toolshop/pageobjects/ProductList.java
@@ -1,4 +1,19 @@
 package org.example.toolshop.pageobjects;
 
+import com.microsoft.playwright.Page;
+
+import java.util.List;
+
 public class ProductList {
+
+    private final Page page;
+
+    public ProductList(Page page) {
+        this.page = page;
+    }
+
+    public List<String> getProductNames() {
+        return page.getByTestId("product-name").allInnerTexts();
+    }
+
 }
