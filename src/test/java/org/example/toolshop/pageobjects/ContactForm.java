89b942a7984383550fc800637f250ed48f0b5385
AkhoMngqibisa <akhomngqibisa@gmail.com>
2025-11-13 19:30:00 +0200

    test - Organise test: Declare variables and initialize on constructor

--- src/test/java/org/example/toolshop/pageobjects/ContactForm.java
+++ src/test/java/org/example/toolshop/pageobjects/ContactForm.java
@@ -1,12 +1,25 @@
 package org.example.toolshop.pageobjects;
 
+import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 
 public class ContactForm {
     private final Page page;
+    private Locator firstNameField;
+    private Locator lastNameField;
+    private Locator emailAddressField;
+    private Locator subjectField;
+    private Locator messageField;
+    private Locator attachmentField;
 
     public ContactForm(Page page) {
         this.page = page;
+        this.firstNameField = page.getByLabel("First name");
+        this.lastNameField = page.getByLabel("Last name");
+        this.emailAddressField = page.getByLabel("Email address");
+        this.subjectField = page.getByLabel("Subject");
+        this.messageField = page.getByLabel("Message");
+        this.attachmentField = page.getByLabel("Attachment");
     }
 
     public void setFirstName(String firstName) {
