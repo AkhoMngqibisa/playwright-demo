a66a81273faf44d7786d304faa25332cee51f2c9
AkhoMngqibisa <akhomngqibisa@gmail.com>
2025-11-15 20:00:00 +0200

    test - Organise test: Take all the files in another repository (HeadlessChromeOptions)

---
+++ src/test/java/org/example/toolshop/HeadlessChromeOptions.java
@@ -0,0 +1,17 @@
+package org.example.toolshop;
+
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.junit.*;
+
+import java.util.Arrays;
+
+public class HeadlessChromeOptions implements OptionsFactory {
+    @Override
+    public Options getOptions() {
+        return new Options().setLaunchOptions(
+                        new BrowserType.LaunchOptions()
+                                .setArgs(Arrays.asList("--no-sandbox", "--disable-extensions", "--disable-gpu"))
+                ).setHeadless(true)
+                .setTestIdAttribute("data-test");
+    }
+}
