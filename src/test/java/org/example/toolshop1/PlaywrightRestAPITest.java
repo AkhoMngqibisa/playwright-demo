0ebe2093c942bc57162d17054f388b25cf7ea415
AkhoMngqibisa <akhomngqibisa@gmail.com>
2025-11-16 20:30:00 +0200

    test - Do configurations on the main class

--- src/test/java/org/example/toolshop1/PlaywrightRestAPITest.java
+++ src/test/java/org/example/toolshop1/PlaywrightRestAPITest.java
@@ -1,8 +1,50 @@
 package org.example.toolshop1;
 
-import org.junit.jupiter.api.Nested;
+import com.microsoft.playwright.*;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.parallel.*;
 
+import java.util.Arrays;
+
+@Execution(ExecutionMode.SAME_THREAD)
 public class PlaywrightRestAPITest {
+
+    protected static Playwright playwright;
+    protected static Browser browser;
+    protected static BrowserContext browserContext;
+
+    Page page;
+
+    @BeforeAll
+    static void setupBrowser() {
+        playwright = Playwright.create();
+        playwright.selectors().setTestIdAttribute("data-test");
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions()
+                .setHeadless(false).setArgs(Arrays.asList("--no-sandbox", "--disable-gpu","disable-extensions"))
+        );
+    }
+
+    @BeforeEach
+    void setup() {
+        browserContext = browser.newContext();
+        page = browserContext.newPage();
+
+        page.navigate("https://practicesoftwaretesting.com");
+        page.getByPlaceholder("Search").waitFor();
+    }
+
+    @AfterEach
+    void tearDown() {
+        browserContext.close();
+        page.close();
+    }
+
+    @AfterAll
+    static void closeBrowser() {
+        browser.close();
+        playwright.close();
+    }
+
     @Nested
     class MakingAPICalls {
 
