0eac2d28cd2257bec1cf82d41b801bb99190228b
AkhoMngqibisa <akhomngqibisa@gmail.com>
2025-11-17 21:30:00 +0200

    test - Create a user record

--- src/test/java/org/example/toolshop/User.java
+++ src/test/java/org/example/toolshop/User.java
@@ -1,4 +1,20 @@
 package org.example.toolshop;
 
-public record User() {
+public record User(
+        String first_name,
+        String last_name,
+        String address,
+        String city,
+        String state,
+        String country,
+        String postcode,
+        String phone,
+        String dob,
+        String password,
+        String email
+) {
+
+    public static User randomUser() {
+        return new User()
+    }
 }
